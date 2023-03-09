25b5effe56cc97e3dbfe573514f80fd027d6c45f
jose chirino <josechirinocarpio@gmail.com>
2023-03-09 12:45:31 -0400

    update to laravel 10

--- routes/v1.php
+++ routes/v1.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\TeamController;
 
 Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('team', TeamController::class);
     Route::apiResource('game', GameController::class);
+    Route::apiResource('team', TeamController::class);
 });
