413a1795dea22e299dc70b666a3eda6493f5ac06
jose chirino <josechirinocarpio@gmail.com>
2023-04-01 14:20:29 -0400

    added limit of balance history records per user

--- app/Models/BalanceHistory.php
+++ app/Models/BalanceHistory.php
@@ -10,10 +10,32 @@ class BalanceHistory extends Model
 {
     use HasFactory;
 
+    const LIMIT_RECORDS_PER_USER = 20;
+
     protected $table = 'user_balance_history';
 
     public function user() : BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected static function booted(): void
+    {
+        static::created(function (BalanceHistory $balanceHistory) {
+            $userLatestBalanceHistory = self::where('user_id', $balanceHistory->user_id)
+                ->latest()
+                ->get();
+
+            // Delete oldest
+            if ($userLatestBalanceHistory->count() > self::LIMIT_RECORDS_PER_USER) {
+                $userLatestBalanceHistoryIds = $userLatestBalanceHistory->take(self::LIMIT_RECORDS_PER_USER)
+                    ->pluck('id')
+                    ->toArray();
+
+                self::where('user_id', $balanceHistory->user_id)
+                    ->whereNotIn('id', $userLatestBalanceHistoryIds)
+                    ->delete();
+            }
+        });
     }
 }
